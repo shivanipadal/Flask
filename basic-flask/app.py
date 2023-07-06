a6fe37f9a9db4bfc07c717b056a9582d3f0d0732
shivaniaspire <shivani.padal@aspiresys.com>
2023-07-06 10:48:22 +0530

    basic flask app

---
+++ basic-flask/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request 
+
+app =  Flask(__name__)
+
+stores = [
+    {
+        'name': 'My wonderfule store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+
+]
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store={
+        'name': request_data['name'],
+        'items': []
+    }
+
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'Store not found'})
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store['name']==name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
+
+
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name: 
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'no item found in store'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
